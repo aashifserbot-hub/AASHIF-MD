61a76a577f59760b8a00fa9d7c18b4b40159e384
aashifserbot-hub <aashifserbot@gmail.com>
2026-03-09 03:18:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+
+export const BOT_NAME = process.env.BOT_NAME || "AASHIF-MD"
+export const OWNER = "AASHIF-MD"
+export const OWNER_NUMBER = process.env.OWNER_NUMBER || "94700000000"
+export const BOT_IMAGE = process.env.BOT_IMAGE || ""
