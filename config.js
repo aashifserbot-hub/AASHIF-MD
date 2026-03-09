cd39080e05802e0d3d49f39f8f93e5c721402ea9
aashifserbot-hub <aashifserbot@gmail.com>
2026-03-09 22:56:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-
-export const BOT_NAME = process.env.BOT_NAME || "AASHIF-MD"
-export const OWNER = "AASHIF-MD"
-export const OWNER_NUMBER = process.env.OWNER_NUMBER || "94768655794"
-export const BOT_IMAGE = process.env.BOT_IMAGE || "https://i.ibb.co/xqtTS4B5/Lte4-Pl-Nbqa.jpg"
+module.exports = {
+    BOT_NAME: process.env.BOT_NAME || 'AASHIF-MD',
+    PREFIX: process.env.PREFIX || '.',
+    OPENAI_KEY: process.env.OPENAI_KEY || '',
+    OMDB_KEY: process.env.OMDB_KEY || ''
+};
